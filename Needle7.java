e9f61cfb3cf9800798af9af438ce884fe1da3b95
babayarova <114256061+babayarova@users.noreply.github.com>
2023-01-18 17:15:49 +0300

    Add files via upload

---
+++ Needle7.java
@@ -0,0 +1,13 @@
+package peaksoft.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+
+
+    @Override
+    public String toString() {
+        return ", а в иголке смерть Кощея :( " + "";
+    }
+}
